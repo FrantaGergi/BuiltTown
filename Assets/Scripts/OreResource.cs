7f6a73bbb0ea12b15acb0a2a9e981e4a1973bcbe
Francis G-Force <panda.gergel@gmail.com>
2025-10-04 18:45:51 +0200

    Add new materials and update scene with new prefabs

---
+++ Assets/Scripts/OreResource.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using static InteractManager;
+
+public class OreResource : Resource, IInteractable
+{
+    protected override void Start()
+    {
+        base.Start();
+
+        miningAnimationName = AnimationType.Mining;
+    }
+
+    protected override void LateStart()
+    {
+        base.LateStart();
+        ItemSO = resourceMapManager.OreSO;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    public override void Interact(InteractManager interactor, InteractAction action)
+    {
+        base.Interact(interactor, action);
+
+
+        switch (action)
+        {
+            case InteractAction.E:
+                break;
+
+            case InteractAction.HoldStart:
+                StartMining();
+                break;
+
+            case InteractAction.Hold:
+                break;
+
+            case InteractAction.HoldEnd:
+                StopMining();
+                break;
+        }
+    }
+
+    public override void OnHoverEnter(InteractManager interactor)
+    {
+        base.OnHoverEnter(interactor);
+
+        if (!HasCorrectTool()) return;
+
+        renderer.material = resourceMapManager.OreSO.HighlightedMaterial;
+    }
+    public override void OnHoverExit()
+    {
+        base.OnHoverExit();
+        renderer.material = originalMaterial;
+    }
+
+    protected override void GetDrop(int ammount)
+    {
+        base.GetDrop(ammount);
+        inventoryManager.AddResourceToHotbar(ItemSO, ammount);
+    }
+
+}
